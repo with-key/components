4a4759ced2becf0cb6275f0edfa24c5ec4158b68
with-key <rem9238@kakao.com>
2022-11-08 01:57:47 +0900

    index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
   return (
-    <>
+    <StContainer>
       <Link href="/select">select</Link>
-    </>
+      <Link href="/table">table</Link>
+    </StContainer>
   );
 };
 
 export default Home;
+
+const StContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 100px;
+`;
